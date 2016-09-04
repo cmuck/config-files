61c9a72ee501ae6daa037c07125f688329f670ae
Christian Muck <cmuckmuck@gmail.com>
2016-09-04 20:28:18 +0200

    Added user question for overwrite to setup script

--- setup.bash
+++ setup.bash
@@ -1,43 +1,67 @@
 #!/bin/bash
 
 set -e
-
-# OS independent files
-echo "Install independent files"
-
-ln -sf "$(pwd)/atom/packages.cson" ~/.atom/packages.cson
-ln -sf "$(pwd)/git/.gitconfig-common" ~/.gitconfig-common
-
-# OS depdenent dotfiles
-OS=$(uname -s)
 GITCONFIG=.gitconfig
+OS=$(uname -s)
 
-echo "Install depdendent files for OS: $OS"
-case $OS in
-    Darwin)
-        cp "$(pwd)/git/.gitconfig-osx" ~/$GITCONFIG
-        ;;
-    Linux)
-        cp "$(pwd)/git/.gitconfig-linux" ~/$GITCONFIG
-        ;;
-    MINGW64*)
-        cp "$(pwd)/git/.gitconfig-windows" ~/$GITCONFIG
-        ;;
-    *)
-        echo "Unsupported operating system. Skipping platform-specific dotfiles."
-esac
+function copyGitConfig
+{
+    OS=$1
+    case $OS in
+        Darwin)
+            cp "$(pwd)/git/.gitconfig-osx" ~/$GITCONFIG
+            ;;
+        Linux)
+            cp "$(pwd)/git/.gitconfig-linux" ~/$GITCONFIG
+            ;;
+        MINGW64*)
+            cp "$(pwd)/git/.gitconfig-windows" ~/$GITCONFIG
+            ;;
+        *)
+            echo "Unsupported operating system. Skipping platform-specific dotfiles."
+    esac
+}
 
-if [ -f ~/$GITCONFIG ]; then
-  if grep "email" ~/$GITCONFIG; then
-    echo "Git already configured with email"
-  else
+function configureGlobalGitAccount
+{
     read -r -p "Enter your name and press [ENTER]: " NAME
     read -r -p "Enter your email and press [ENTER]: " EMAIL
     git config --global user.name "$NAME"
     git config --global user.email "$EMAIL"
-  fi
+}
+
+
+# OS independent files
+echo "Install independent OS files"
+
+ln -sf "$(pwd)/atom/packages.cson" ~/.atom/packages.cson
+ln -sf "$(pwd)/git/.gitconfig-common" ~/.gitconfig-common
+
+
+# OS depdenent dotfiles
+echo "Install depdendent OS files for: $OS"
+
+
+if [ -f ~/$GITCONFIG ]; then
+    read -r -p "$GITCONFIG already exists, do you want to overwrite it (y/n)?" OVERWRITE
+
+    case $OVERWRITE in
+        "y"|"Y")
+            copyGitConfig $OS
+            ;;
+        "n"|"N")
+            echo "$GITCONFIG not overwritten"
+            ;;
+    esac
 else
-   echo "File $GITCONFIG does not exist"
+    echo "$GITCONFIG does not exist, copy new Git config"
+    copyGitConfig $OS
+fi
+
+if grep "email" ~/$GITCONFIG &> /dev/null ; then
+    echo "Git already configured with email"
+else
+    configureGlobalGitAccount
 fi
 
 echo "Finished setup"
