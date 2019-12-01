6e3ed424bc8d38ac160af4eb283750973dc1f991
Christian Muck <cmuckmuck@gmail.com>
2019-12-01 11:52:15 +0100

    Applied shfmt

--- setup.bash
+++ setup.bash
@@ -15,8 +15,7 @@ OVERWRITE=
 VSCODE_PROGRAM=code
 ZSH_PROGRAM=zsh
 
-linkAtomPackages()
-{
+linkAtomPackages() {
     ATOM_DIR=~/.atom
     if [ ! -d "$ATOM_DIR" ]; then
         mkdir $ATOM_DIR
@@ -26,20 +25,17 @@ linkAtomPackages()
     echo " $ATOM_PROGRAM finished"
 }
 
-configureGitUserAndMail ()
-{
-    if grep "email" ~/$GIT_CONFIG &> /dev/null ; then
+configureGitUserAndMail() {
+    if grep "email" ~/$GIT_CONFIG &>/dev/null; then
         echo "Git already configured with email"
     else
         # If NAME is empty, request from user
-        if [ -z "$NAME" ]
-        then
+        if [ -z "$NAME" ]; then
             read -r -p "Enter your name and press [ENTER]: " NAME
         fi
 
         # If EMAIL is empty, request from user
-        if [ -z "$EMAIL" ]
-        then
+        if [ -z "$EMAIL" ]; then
             read -r -p "Enter your email and press [ENTER]: " EMAIL
         fi
 
@@ -51,8 +47,7 @@ configureGitUserAndMail ()
     fi
 }
 
-copyGitIndependentOS ()
-{
+copyGitIndependentOS() {
     GIT_DIR=~/.git
     if [ ! -d "$GIT_DIR" ]; then
         mkdir $GIT_DIR
@@ -62,55 +57,52 @@ copyGitIndependentOS ()
     echo " $GIT_PROGRAM common finished"
 }
 
-copyGitConfig ()
-{
+copyGitConfig() {
     case $OS in
-        Darwin)
-            cp "$(pwd)/git/.gitconfig-osx" ~/$GIT_CONFIG
-            ;;
-        Linux)
-            cp "$(pwd)/git/.gitconfig-linux" ~/$GIT_CONFIG
-            ;;
-        MINGW64*)
-            cp "$(pwd)/git/.gitconfig-windows" ~/$GIT_CONFIG
-            ;;
-        *)
-            echo "Unsupported operating system. Skipping platform-specific dotfiles."
-            ;;
+    Darwin)
+        cp "$(pwd)/git/.gitconfig-osx" ~/$GIT_CONFIG
+        ;;
+    Linux)
+        cp "$(pwd)/git/.gitconfig-linux" ~/$GIT_CONFIG
+        ;;
+    MINGW64*)
+        cp "$(pwd)/git/.gitconfig-windows" ~/$GIT_CONFIG
+        ;;
+    *)
+        echo "Unsupported operating system. Skipping platform-specific dotfiles."
+        ;;
     esac
 
     echo " git config copied"
 }
 
-copyGitDependentOS ()
-{
+copyGitDependentOS() {
     # Chech if git config is present
     if [ -f ~/$GIT_CONFIG ]; then
         # If OVERWRITE is empty, request from user
-        if [ -z "$OVERWRITE" ]
-        then
+        if [ -z "$OVERWRITE" ]; then
             while read -r -p "$GIT_CONFIG already exists, do you want to overwrite it (y/n)?" OVERWRITE; do
                 case "$OVERWRITE" in
-                    [Yy]*)
-                        echo "$GIT_CONFIG will be overwritten"
-                        break
+                [Yy]*)
+                    echo "$GIT_CONFIG will be overwritten"
+                    break
                     ;;
-                    [Nn]*)
-                        break
+                [Nn]*)
+                    break
                     ;;
-                    *)
-                        echo "Invalid option" >&2
+                *)
+                    echo "Invalid option" >&2
                     ;;
                 esac
             done
         fi
 
         case $OVERWRITE in
-            [Yy]*)
-                copyGitConfig
+        [Yy]*)
+            copyGitConfig
             ;;
-            [YNn*)
-                echo "$GIT_CONFIG will be kept"
+        [YNn*)
+            echo "$GIT_CONFIG will be kept"
             ;;
         esac
     else
@@ -121,26 +113,22 @@ copyGitDependentOS ()
     echo " $GIT_PROGRAM finished"
 }
 
-configureGit()
-{
-   copyGitIndependentOS
-   copyGitDependentOS
-   configureGitUserAndMail
+configureGit() {
+    copyGitIndependentOS
+    copyGitDependentOS
+    configureGitUserAndMail
 }
 
-installVSCodeExtensions ()
-{
-    while IFS= read -r extension
-    do
+installVSCodeExtensions() {
+    while IFS= read -r extension; do
         echo " -- Installing $extension"
-        code --install-extension "$extension" > /dev/null
-    done  < <(grep -v '^ *#' <  vscode/extensions.txt)
+        code --install-extension "$extension" >/dev/null
+    done < <(grep -v '^ *#' <vscode/extensions.txt)
 
     echo " $VSCODE_PROGRAM finished"
 }
 
-linkZshConfig ()
-{
+linkZshConfig() {
     if [ -f ~/.zshrc ]; then
         rm ~/.zshrc
     fi
@@ -150,10 +138,9 @@ linkZshConfig ()
     echo " $ZSH_PROGRAM finished"
 }
 
-usage ()
-{
+usage() {
 
-cat << EOF
+    cat <<EOF
 Usage: $0 options
 
 This script helps to setup config-files for development using
@@ -172,64 +159,62 @@ EOF
 }
 
 while getopts "kohn:e:" opt; do
-  case ${opt} in
-    o )
+    case ${opt} in
+    o)
         # Will overwrite the git config
         OVERWRITE=Y
-      ;;
-    k )
+        ;;
+    k)
         # Will overwrite the git config
         OVERWRITE=N
-      ;;
-    n )
+        ;;
+    n)
         # Git user name
         NAME=$OPTARG
-      ;;
-    e )
+        ;;
+    e)
         # Git user email
         EMAIL=$OPTARG
-      ;;
+        ;;
     h)
         usage
         exit 0
-      ;;
-    \? )
+        ;;
+    \?)
         # Wrong option
         usage
         exit 1
-      ;;
-    : )
+        ;;
+    :)
         # Right option with invalid argument
         echo "Invalid option: $OPTARG requires an argument" 1>&2
         usage
         exit 1
-    ;;
-  esac
+        ;;
+    esac
 done
 
-checkForPrograms()
-{
-  echo ""
-  echo "Check program installation ..."
-  ATOM_INSTALLED=$(command -v $ATOM_PROGRAM) || true
-  GIT_INSTALLED=$(command -v $GIT_PROGRAM) || true
-  VSCODE_INSTALLED=$(command -v $VSCODE_PROGRAM) || true
-  ZSH_INSTALLED=$(command -v $ZSH_PROGRAM) || true
+checkForPrograms() {
+    echo ""
+    echo "Check program installation ..."
+    ATOM_INSTALLED=$(command -v $ATOM_PROGRAM) || true
+    GIT_INSTALLED=$(command -v $GIT_PROGRAM) || true
+    VSCODE_INSTALLED=$(command -v $VSCODE_PROGRAM) || true
+    ZSH_INSTALLED=$(command -v $ZSH_PROGRAM) || true
 
-  echo "$ATOM_PROGRAM: $ATOM_INSTALLED"
-  echo "$GIT_PROGRAM: $GIT_INSTALLED"
-  echo "$VSCODE_PROGRAM: $VSCODE_INSTALLED"
-  echo "$ZSH_PROGRAM: $ZSH_INSTALLED"
+    echo "$ATOM_PROGRAM: $ATOM_INSTALLED"
+    echo "$GIT_PROGRAM: $GIT_INSTALLED"
+    echo "$VSCODE_PROGRAM: $VSCODE_INSTALLED"
+    echo "$ZSH_PROGRAM: $ZSH_INSTALLED"
 }
 
-configureProgram()
-{
-  echo "Configure $1"
-  if [ -n "$2" ]; then
-      $3
-  else
-      echo " $1 is not installed, skipping setup ..."
-  fi
+configureProgram() {
+    echo "Configure $1"
+    if [ -n "$2" ]; then
+        $3
+    else
+        echo " $1 is not installed, skipping setup ..."
+    fi
 
 }
 
