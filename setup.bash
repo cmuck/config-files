2d7291e0197058b28052ef7d669118df9a440e1c
cmuck <cmuckmuck@gmail.com>
2022-07-17 19:06:12 +0200

    Added Traivs, added Getopts, updated Atom packages

--- setup.bash
+++ setup.bash
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 set -e
+ATOMDIR=~/.atom
+EMAIL=
 GITCONFIG=.gitconfig
+GITDIR=~/.git
+NAME=
 OS=$(uname -s)
+OVERWRITE=
 
 function copyGitConfig
 {
@@ -24,17 +29,65 @@ function copyGitConfig
 
 function configureGlobalGitAccount
 {
-    read -r -p "Enter your name and press [ENTER]: " NAME
-    read -r -p "Enter your email and press [ENTER]: " EMAIL
+    # If NAME is empty, request from user
+    if [ -z "$NAME" ]
+    then
+        read -r -p "Enter your name and press [ENTER]: " NAME
+    fi
+
+    # If EMAIL is empty, request from user
+    if [ -z "$EMAIL" ]
+    then
+        read -r -p "Enter your email and press [ENTER]: " EMAIL
+    fi
+
+    # Set global git config
     git config --global user.name "$NAME"
     git config --global user.email "$EMAIL"
 }
 
 
+while getopts ":yn:e:" opt; do
+  case ${opt} in
+    y )
+        # Will overwrite the git config
+        OVERWRITE=Y
+      ;;
+    n )
+        # Git user name
+        NAME=$OPTARG
+      ;;
+    e )
+        # Git user email
+        EMAIL=$OPTARG
+      ;;
+    \? )
+        # Wrong option
+        echo "Usage: cmd [-n NAME] [-e EMAIL]"
+        exit -1
+      ;;
+    : )
+        # Right option with invalid argument
+        echo "Invalid option: $OPTARG requires an argument" 1>&2
+        exit -1
+    ;;
+  esac
+done
+
+
 # OS independent files
 echo "Install independent OS files"
 
+# Atom
+if [ ! -d "$ATOMDIR" ]; then
+  mkdir $ATOMDIR
+fi
 ln -sf "$(pwd)/atom/packages.cson" ~/.atom/packages.cson
+
+# Git
+if [ ! -d "$GITDIR" ]; then
+    mkdir $GITDIR
+fi
 ln -sf "$(pwd)/git/.gitconfig-common" ~/.gitconfig-common
 ln -sf "$(pwd)/git/.git_commit_message.txt" ~/.git_commit_message.txt
 
@@ -42,9 +95,14 @@ ln -sf "$(pwd)/git/.git_commit_message.txt" ~/.git_commit_message.txt
 # OS depdenent dotfiles
 echo "Install depdendent OS files for: $OS"
 
-
+# Chech if git config is present
 if [ -f ~/$GITCONFIG ]; then
-    read -r -p "$GITCONFIG already exists, do you want to overwrite it (y/n)?" OVERWRITE
+
+    # If OVERWRITE is empty, request from user
+    if [ -z "$OVERWRITE" ]
+    then
+        read -r -p "$GITCONFIG already exists, do you want to overwrite it (y/n)?" OVERWRITE
+    fi
 
     case $OVERWRITE in
         "y"|"Y")
