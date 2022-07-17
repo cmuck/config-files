e155eae909fb85722a57d3623bf2eabaa3665349
cmuck <cmuckmuck@gmail.com>
2022-07-17 19:06:38 +0200

    New clion version, minor i3 changes

--- roles/i3/files/local/bin/statusbar/disk.sh
+++ roles/i3/files/local/bin/statusbar/disk.sh
@@ -8,22 +8,22 @@ THRESHOLD_CRITICAL=80
 
 # 1 to 3 for left/middle/right mouse button
 case "${BLOCK_BUTTON}" in
-	1) notify-send "💽Disk space" "$(df -h --output=target,used,size)" ;;
-	3) notify-send "Disk status" "Shows disk space [used]/[total] [% used]
+  1) notify-send "💽Disk space" "$(df -h --output=target,used,size)" ;;
+  3) notify-send "Disk status" "Shows disk space [used]/[total] [% used]
 - Left click to show all disk info
 - Middle none
 - Right click none" ;;
 esac
 
 directory="${BLOCK_INSTANCE:-/}"
-df_dump="$( df -h -P -l "${directory}" | tail -1 )"
+df_dump="$(df -h -P -l "${directory}" | tail -1)"
 
-total="$( echo "${df_dump}" | awk '{ print $2}' | grep -o '[.0-9]*' || true )"
-used="$( echo "${df_dump}"  | awk '{ print $3}' | grep -o '[.0-9]*' || true )"
-free="$( echo "${df_dump}"  | awk '{ print $4}' | grep -o '[.0-9]*' || true )"
+total="$(echo "${df_dump}" | awk '{ print $2}' | grep -o '[.0-9]*' || true)"
+used="$(echo "${df_dump}" | awk '{ print $3}' | grep -o '[.0-9]*' || true)"
+free="$(echo "${df_dump}" | awk '{ print $4}' | grep -o '[.0-9]*' || true)"
 
-percent_used="$( echo "${df_dump}" | awk '{ print $5}' | sed 's/%//g' )"
-percent_free="$(( 100 - percent_used ))"
+percent_used="$(echo "${df_dump}" | awk '{ print $5}' | sed 's/%//g')"
+percent_free="$((100 - percent_used))"
 
 #echo "total: $total"
 #echo "used: $used"
@@ -37,8 +37,8 @@ echo "${used}G/${total}G ${percent_used}%"
 echo "${used}G/${total}G ${percent_used}%"
 
 if [[ "${percent_used}" -ge "${THRESHOLD_CRITICAL}" ]]; then
-    echo "#FF0000"
-    exit 33
+  echo "#FF0000"
+  exit 33
 elif [[ "${percent_used}" -ge "${THRESHOLD_WARNING}" ]]; then
-    echo "#FFFC00"
+  echo "#FFFC00"
 fi
