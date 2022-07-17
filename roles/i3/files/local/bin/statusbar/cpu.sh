488f4034d0f92e8087fc92bceda3d36abd55e9c1
cmuck <cmuckmuck@gmail.com>
2022-07-17 19:06:38 +0200

    Configured i3blocks statusbar

---
+++ roles/i3/files/local/bin/statusbar/cpu.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Prints CPU usage with a warning (yellow color) and critical (red color) threshold
+
+THRESHOLD_WARNING=50
+THRESHOLD_CRITICAL=80
+
+ERROR_MESSAGE="i3blocks default module missing"
+
+# 1 to 3 for left/middle/right mouse button
+case "${BLOCK_BUTTON}" in
+	1) notify-send "🖥CPU hogs" "$(ps axch -o cmd:15,%cpu --sort=-%cpu | head)\\n(100% per core)" ;;
+	2) setsid "$TERMINAL" -e htop & ;;
+	3) notify-send "CPU status" "Shows CPU details
+- Left click to show intensive processes
+- Middle click to open htop
+- Right click none" ;;
+esac
+
+I3_BLOCKS_DEFAULT="/usr/share/i3blocks/cpu_usage"
+
+if [[ ! -e "${I3_BLOCKS_DEFAULT}" ]]; then
+    # i3blocks full_text
+    echo "${ERROR_MESSAGE}"
+    # i3blocks full_text
+    echo "${ERROR_MESSAGE}"
+    exit 33
+fi
+
+"${I3_BLOCKS_DEFAULT}" -w "${THRESHOLD_WARNING}" -c "${THRESHOLD_CRITICAL}"
---
+++ roles/i3/files/local/bin/statusbar/iface.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Prints network interface details
+
+INTERFACE=$(ip route | awk '/^default/ { print $5 }')
+INTERFACE_DOWN_MESSAGE="IPv4: Down"
+
+INET=$(ip addr show enp0s3 | awk '/^.*inet.*\/24/ {print substr($2, 1, length($2)-3)}')
+INET6=$(ip addr show enp0s3 | awk '/^.*inet6.*\/64/ {print substr($2, 1, length($2)-3)}')
+
+# 1 to 3 for left/middle/right mouse button
+case "${BLOCK_BUTTON}" in
+	#1) notify-send "Network interfaces" "Interface (default): ${INTERFACE}\nIPv4: ${INET}\nIPv6: ${INET6}" ;;
+	1) notify-send "Network interfaces" "$(ip addr show)" ;;
+	2) echo -n "${INET:-$INTERFACE_DOWN_MESSAGE}" | xclip -q -se c ;;
+	3) notify-send "Network interface status" "Shows network interface details
+- Click to show network interface details
+- Middle click to copy IPv4 adress
+- Right click none" ;;
+esac
+
+if [[ -z ${INTERFACE} ]]; then
+  echo "${INTERFACE_DOWN_MESSAGE}"
+  exit 33;
+fi
+
+# i3blocks full_text
+echo "IPv4 ${INET}"
+# i3blocks full_text
+echo "IPv4 ${INET}"
